f5c7f8c9ad39d2e2c87cd9cbec16bad5211fb015
Pannio <1821299140@qq.com>
2024-01-03 01:13:45 +0800

    clearId to clerkId

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,9 +1,9 @@
 import { Schema, models, model } from "mongoose"
 
 const UserSchema = new Schema({
-  clearId: { type: String, required: true, unique: true },
+  clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  username: { type: String, required:true, unique: true },
+  username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   photo: { type: String, required: true },
